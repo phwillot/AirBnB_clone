cff6328a64a4ebb1173cfb25158dee57c79c5a34
WILLOT Philippe <willot.ph@gmail.com>
2022-03-02 12:49:57 +0000

    Adding file_storage class

--- models/base_model.py
+++ models/base_model.py
@@ -2,15 +2,24 @@
 """Base class that defines all common attributes/methods for other classes"""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
     """This class defines all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
         """Initilization of the instance of the class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
+            for key, val in kwargs.items():
+                if key != '__class__':
+                    if key != 'created_at' and key != 'updated_at':
+                        setattr(self, key, val)
+                    else:
+                        setattr(self, key, datetime.fromisoformat(val))
 
     def __str__(self):
         """Change the representation of the object"""
@@ -22,13 +31,13 @@ class BaseModel:
         """updates the public instance attribute updated_at
         with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of
         __dict__ of the instance"""
-        createdISO = datetime.isoformat(self.created_at)
-        updatedISO = datetime.isoformat(self.updated_at)
-        self.__dict__["__class__"] = self.__class__.__name__
-        self.__dict__["created_at"] = createdISO
-        self.__dict__["updated_at"] = updatedISO
-        return self.__dict__
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = dictionary["created_at"].isoformat()
+        dictionary['updated_at'] = dictionary["updated_at"].isoformat()
+        return dictionary
---
+++ models/engine/file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""FileStorage that serializes instances to a JSON file and
+deserializes JSON file to instances:"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """class FileStorage:
+    serializes instance to a JSON file and deserializes
+    JSON file to instances"""
+
+    def __init__(self):
+        """initialization"""
+        self.__file_path = "file.json"
+        self.__objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key
+        <obj class name>.id"""
+        self.__objects.update({f"{obj.__class__.__name__}.{obj.id}": obj})
+
+    def save(self):
+        """serializes __objects to JSON file"""
+        dict_json = {}
+        for key in self.__objects.keys():
+            dict_json[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, "w+", encoding="utf-8") as file:
+            json.dump(dict_json, file, indent=4)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects."""
+        try:
+            with open(self.__file_path, "r+") as file:
+                dictJson = json.load(file)
+                for key, value in dictJson.items():
+                    self.__objects[key] = eval(value["__class__"])(**value)
+        except Exception as e:
+            pass
